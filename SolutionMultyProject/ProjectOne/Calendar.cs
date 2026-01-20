aaf7975304d94233eb9f3f9c4a13618cf4270baa
Denis <konovalovdenis612@gmail.com>
2026-01-20 16:01:59 +0300

    Add interface

---
+++ SolutionMultyProject/ProjectOne/Calendar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ProjectOne
+{
+    public class Calendar
+    {
+         public static void Main()
+        {
+            DateTime now = GetCurrentDate();
+            Console.WriteLine($"Today's date is {now}");
+            Console.ReadLine();
+        }
+
+    static DateTime GetCurrentDate()
+    {
+        return DateTime.Now.Date;
+    }
+
+    
+    }
+}
